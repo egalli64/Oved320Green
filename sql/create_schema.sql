ed39eea008e64f03d9adee3c09dbf4dbf096fbec
Angelica <angelica_barrion@yahoo.it>
2020-04-17 16:46:53 +0200

    create green

---
+++ sql/create_schema.sql
@@ -0,0 +1,5 @@
+create user green identified by 'password'; -- password delimitata da apici e case sensitive
+create schema green; -- ambito in cui sono definiti oggetti correlati (in MySQL è sinonimo di database)
+grant all privileges on green.* to green; -- tutti i privilegi standard sullo schema me all’utente me
+grant alter routine on green.* to green; -- privilegi per modificare le procedure
+-- drop user me --
