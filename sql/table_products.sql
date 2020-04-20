392a2f8780c842fb8dab757c93cec2524903b5fb
cristina <cristina.roca92@gmail.com>
2020-04-20 11:04:43 +0200

    commit tabella prodotti

---
+++ sql/table_products.sql
@@ -0,0 +1,24 @@
+use green;
+create table products
+(
+
+product_id int primary key not null,
+product_name varchar(25) not null,
+price_money null
+);
+
+select * from products;
+
+insert into products (product_id,product_name, price_money) 
+values ('1','Scaldatelli','2.50');
+insert into products (product_id,product_name, price_money) 
+values ('2', 'Biscotti da Latte', '3.00');
+insert into products (product_id,product_name,price_money) 
+values ('3', ' Uova' '2.00');
+insert into product (product_id,product_name,price_money)  
+values ('4', 'Carote','1.50');
+insert into product (product_id,product_name,price_money)
+values ('5', 'Pasta di Mandorle', '10.00');
+
+
+
