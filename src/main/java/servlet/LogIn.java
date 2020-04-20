daec06d111c529310cb6644f0885f773cb52b405
Manny <egalli64@gmail.com>
2020-04-20 16:53:20 +0200

    code review

--- src/main/java/servlet/LogIn.java
+++ src/main/java/servlet/LogIn.java
@@ -1,13 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
