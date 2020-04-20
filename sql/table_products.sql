83f6ed78994cf173341c60123f2263b6c5527e0c
Angelica <angelica_barrion@yahoo.it>
2020-04-20 11:16:34 +0200

    tabella prodotti CORRETTA

--- sql/table_products.sql
+++ sql/table_products.sql
@@ -1,10 +1,11 @@
 use green;
+drop table products ;
 create table products
 (
 
 product_id int primary key not null,
 product_name varchar(25) not null,
-price_money decimal 
+price_money decimal(6,2) not null
 );
 
 select * from products;
@@ -14,11 +15,11 @@ values ('1','Scaldatelli','2.50');
 insert into products (product_id,product_name, price_money) 
 values ('2', 'Biscotti da Latte', '3.00');
 insert into products (product_id,product_name,price_money) 
-values ('3', ' Uova' '2.00');
-insert into product (product_id,product_name,price_money)  
+values ('3', ' Uova', '2.00');
+insert into products (product_id,product_name,price_money)  
 values ('4', 'Carote','1.50');
-insert into product (product_id,product_name,price_money)
+insert into products (product_id,product_name,price_money)
 values ('5', 'Pasta di Mandorle', '10.00');
 
-
+select * from products;
 
