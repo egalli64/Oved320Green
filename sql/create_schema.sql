a5fa96880a954af26c511776afe0648102e2f5fc
Manny <egalli64@gmail.com>
2020-04-21 09:09:52 +0200

    if exists

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,7 +1,9 @@
-create user green identified by 'password'; -- password delimitata da apici e case sensitive
-create schema green; -- ambito in cui sono definiti oggetti correlati (in MySQL è sinonimo di database)
-grant all privileges on green.* to green; -- tutti i privilegi standard sullo schema me all’utente me
-grant alter routine on green.* to green; -- privilegi per modificare le procedure
--- drop user me --
--- drop user green;
--- drop schema green;
+-- run this as root
+
+drop user if exists green;
+drop schema if exists green;
+
+create user green identified by 'password';
+create schema green;
+grant all privileges on green.* to green;
+grant alter routine on green.* to green;
